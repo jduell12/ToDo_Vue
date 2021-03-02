e5dc96397fd27a39224eb871c421e454951384ab
Jessica <duellje@oregonstate.edu>
2021-03-02 18:50:46 -0500

    made add/edit/delete tasks so they send requests to fake db

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -33,19 +33,37 @@ export default {
     }
   }, 
   methods: {
-    deleteTask(id){
+    async deleteTask(id){
       if(confirm('Are you sure?')){
-        this.tasks = this.tasks.filter(task => task.id !== id)
+        const res = await fetch(`api/tasks/${id}`,{
+          method: 'DELETE'
+        })
+
+        res.status === 200 ? (this.tasks = this.tasks.filter(task => task.id !== id)) : alert('Error deleting task')
+        
       }
     }, 
-    toggleReminder(id){
+    async toggleReminder(id){
+      const taskToToggle = await this.fetchTask(id)
+      const updateTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+      
+      const res = await fetch(`api/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        }, 
+        body: JSON.stringify(updateTask)
+      })
+
+      const data = await res.json()
+
       this.tasks = this.tasks.map(task => {
         //another way to change the reminder for a task
         // if(task.id === id){
         //   task.reminder = !task.reminder
         // }
         // return task
-        return task.id === id ? {...task, reminder: !task.reminder} : task
+        return task.id === id ? {...task, reminder: data.reminder} : task
       } )
     }, 
     async addTask(task){
