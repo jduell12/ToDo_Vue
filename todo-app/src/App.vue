6752b81197ad67320325b8c2b7fbfee3e88c2d6b
Jessica <duellje@oregonstate.edu>
2021-03-02 19:02:46 -0500

    added router link so there would not be a refresh when going to about page

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -10,12 +10,15 @@
       <AddTask @add-task="addTask" />
     </div>
     <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
+    <Footer />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
@@ -23,6 +26,7 @@ export default {
   name: 'App',
   components: {
     Header,
+    Footer, 
     Tasks,
     AddTask
   },
